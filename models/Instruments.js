75b31a5a8d5cf024b5e7b198078ac90ddfa72ea9
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-05-09 11:32:11 -0600

    finished Instruments model

--- models/Instruments.js
+++ models/Instruments.js
@@ -3,4 +3,30 @@ const sequelize = require('../config/connection');
 
 class Instruments extends Model {};
 
+Instruments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.VARCHAR(30),
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    }
+)
+
 module.exports = Instruments;
