2673d7d46f05c90953a65f86b5d3ee39b1ba4cd5
aarticontractor <contaaru@gmail.com>
2023-05-10 10:20:58 -0700

    Added Upload APIs for all Media Type

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const musicianRoutes = require('./musicianRoutes');
 const instrumentRoutes = require('./instrumentRoutes');
 // const songRoutes = require('./songRoutes');
 // const gigRoutes = require('./gigRoutes');
+const mediaRoutes = require('./mediaRoutes');
 
 router.use('/musician', musicianRoutes);
 router.use('/instrument', instrumentRoutes);
 // router.use('/song', songRoutes);
 // router.use('/gig', gigRoutes);
+router.use('/media', mediaRoutes);
 
 module.exports = router;
