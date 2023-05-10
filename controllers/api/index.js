2ea8d37ce60dfc8e0fe3c7a14b3780cd34acbf0f
aarticontractor <contaaru@gmail.com>
2023-05-10 16:34:36 -0700

    Fix song routes in controller/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const musicianRoutes = require('./musicianRoutes');
 const instrumentRoutes = require('./instrumentRoutes');
-// const songRoutes = require('./songRoutes');
+const songRoutes = require('./songRoutes');
 // const gigRoutes = require('./gigRoutes');
-const mediaRoutes = require('./mediaRoutes');
+// const mediaRoutes = require('./mediaRoutes');
 
 router.use('/musician', musicianRoutes);
 router.use('/instrument', instrumentRoutes);
-// router.use('/song', songRoutes);
+router.use('/song', songRoutes);
 // router.use('/gig', gigRoutes);
-router.use('/media', mediaRoutes);
+// router.use('/media', mediaRoutes);
 
 module.exports = router;
