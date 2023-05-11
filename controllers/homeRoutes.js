cf73a285bb6af5b38b7e75e505b1739ca4ee2fb0
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-05-11 13:13:00 -0600

    added protectors so user cannot manipulate other users' data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -80,7 +80,8 @@ router.get('/musician/:id', withAuth, async (req, res) => {
     const musician = musicianData.get({ plain: true });
     res.render('profile', { 
         musician,
-        logged_in: req.session.logged_in 
+        logged_in: req.session.logged_in,
+        is_user: false
     });
     } catch (err) {
         console.log(err);
@@ -136,7 +137,8 @@ router.get('/profile', withAuth, async (req, res) => {
         console.log(user);
         res.render('profile', {
             ...user,
-            logged_in: true
+            logged_in: req.session.logged_in,
+            is_user: true
         });
     } catch (err) {
       res.status(500).json(err);
